3ed590264f6f80597f136a64427b518b76331a44
Marcin Kozicki <m.kozicki2@gmail.com>
2019-08-17 14:04:43 +0200

    Display dashboard for Polish population

--- src/App.js
+++ src/App.js
@@ -5,24 +5,42 @@ import Population from './components/population/Population';
 const path = 'https://datahub.io/core/population/r/population.json';
 const prefix = "https://cors-anywhere.herokuapp.com/";
 class App extends React.Component {
-  
+
   state = {
-    
+    population: 'POL',
+    populationData: []
   }
 
-getData = () =>{
-  fetch(prefix + path)
-  .then(res=> res.json())
-  .then(data=> console.log(data[400]))
+  getData = () => {
+    fetch(prefix + path)
+      .then(res => res.json())
+      .then(data => this.parsePopulationData(data))
+      .catch(err => console.log(err))
+  }
 
-}
+  parsePopulationData = (data) => {
+    const populationData = data.filter(item => item['Country Code'] === this.state.population);
+    const parsedData = [];
 
-  render(){
+    populationData.forEach(element => {
+      parsedData.push({
+        'x': element.Year,
+        'y': element.Value / 1000000
+      })
+    });
+    this.setState({
+      populationData: parsedData
+    })
+  }
+
+  render() {
+    console.log(this.state);
 
 
     return (
-      <div onClick={this.getData} className="App">
-        <Population />
+      <div className="App">
+        <header onClick={this.getData}>Data Dashboard</header>
+        <Population data={this.state} />
       </div>
     );
   }
--- src/components/population/Population.js
+++ src/components/population/Population.js
@@ -1,14 +1,99 @@
 import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
+import './Population.css'
 
-class Population extends React.Component {
-    render(){
-        return(
-<div>
-    population dsf
-</div>
-        )
-    }
+// const data = [
 
+//     {
+//         "id": "japan",
+//         "color": "hsl(67, 70%, 50%)",
+//         "data": [
+//             {
+//                 x: 2000,
+//                 y: 242
+//             },
+//             {
+//                 "x": 2001,
+//                 "y": 200
+//             },
+
+//         ]
+//     }]
+const Population = (props) => {
+
+    let dataToDisplay = [
+        {
+            "id": "Poland",
+            "color": "hsl(67, 70%, 50%)",
+            "data": props.data.populationData === undefined ? [{ x: 0, y: 0 }] : props.data.populationData
+        }
+    ]
+
+    return (
+        <div className='container'>
+            <ResponsiveLine
+                data={dataToDisplay}
+                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                xScale={{ type: 'point' }}
+                yScale={{ type: 'linear', stacked: true, min: 'auto', max: 'auto' }}
+                axisTop={null}
+                axisRight={null}
+                axisBottom={{
+                    orient: 'bottom',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: -60,
+                    legend: 'year',
+                    legendOffset: 40,
+                    legendPosition: 'middle'
+                }}
+                axisLeft={{
+                    orient: 'left',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: 'population (mln)',
+                    legendOffset: -40,
+                    legendPosition: 'middle'
+                }}
+                colors={{ scheme: 'nivo' }}
+                enablePoints={false}
+                pointSize={10}
+                pointColor={{ theme: 'background' }}
+                pointBorderWidth={2}
+                pointBorderColor={{ from: 'serieColor' }}
+                pointLabel="y"
+                pointLabelYOffset={-12}
+                useMesh={true}
+                legends={[
+                    {
+                        anchor: 'bottom-right',
+                        direction: 'column',
+                        justify: false,
+                        translateX: 100,
+                        translateY: 0,
+                        itemsSpacing: 0,
+                        itemDirection: 'left-to-right',
+                        itemWidth: 80,
+                        itemHeight: 20,
+                        itemOpacity: 0.75,
+                        symbolSize: 12,
+                        symbolShape: 'circle',
+                        symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                        effects: [
+                            {
+                                on: 'hover',
+                                style: {
+                                    itemBackground: 'rgba(0, 0, 0, .03)',
+                                    itemOpacity: 1
+                                }
+                            }
+                        ]
+                    }
+                ]}
+            />
+        </div>
+    )
 }
 
 export default Population;
