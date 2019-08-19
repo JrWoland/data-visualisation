c348bcdea39e1cfa73245018acc4921205f3ede6
Marcin Kozicki <m.kozicki2@gmail.com>
2019-08-19 18:58:17 +0200

    Updates

---
+++ precache-manifest.53f39384bb6598876b95cc61b9a3f305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89d9359396769524f5d6ad5db1f70d1",
+    "url": "/data-visualisation/index.html"
+  },
+  {
+    "revision": "fa9728cfa90251abc4df",
+    "url": "/data-visualisation/static/css/main.a8e7d05f.chunk.css"
+  },
+  {
+    "revision": "1023ec5f36a54160e7d7",
+    "url": "/data-visualisation/static/js/2.e20c7080.chunk.js"
+  },
+  {
+    "revision": "fa9728cfa90251abc4df",
+    "url": "/data-visualisation/static/js/main.7ea06e47.chunk.js"
+  },
+  {
+    "revision": "0f473b052c44dd3b4b5c",
+    "url": "/data-visualisation/static/js/runtime~main.f35c68f7.js"
+  }
+]);
